fc154cd0ba7165b1a41a223c0050afcb0ba337c3
Lucas Dias <diasc.lucas@gmail.com>
2020-03-30 19:27:23 -0300

    Ordenando listagem do histórico

--- search/src/main/java/com/lucasdias/search/data/historic/local/SearchHistoricCache.kt
+++ search/src/main/java/com/lucasdias/search/data/historic/local/SearchHistoricCache.kt
@@ -3,21 +3,26 @@ package com.lucasdias.search.data.historic.local
 import android.content.Context
 import androidx.preference.PreferenceManager
 import com.lucasdias.base.typeconverter.TypeConverter
+import java.util.*
+import kotlin.collections.ArrayList
 
 internal class SearchHistoricCache(private var context: Context) {
 
     private companion object {
         const val SEARCH_HISTORIC_KEY = "SEARCH_HISTORIC_KEY"
         const val EMPTY_STRING = ""
+        const val DO_NOT_CONTAIN = -1
+        const val FIRST_POSITION = 0
     }
 
     fun setSearch(search: String) {
 
         val historic = getHistoric()
-        val searchTextAlreadyExist = historic.contains(search)
-        if (searchTextAlreadyExist) return
+        val indexOfContainedString = historic.indexOf(search.trim())
+        if (indexOfContainedString != DO_NOT_CONTAIN) historic.removeAt(indexOfContainedString)
 
-        historic.add(search)
+        historic.add(FIRST_POSITION, search)
+
         val historicAsString = TypeConverter.arrayListToString(historic)
 
         val preferences = PreferenceManager.getDefaultSharedPreferences(context)
